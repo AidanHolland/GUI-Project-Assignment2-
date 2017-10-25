73bbb267b042dfb6fbe6fdf2d3aac77f8f988987
Aidan Holland <aidanjames11@hotmail.com>
2017-10-25 22:34:40 +1100

    ff

--- src/View/Inventory.java
+++ src/View/Inventory.java
@@ -1,8 +1,17 @@
 package View;
+import javax.swing.*;
 
-public class Inventory 
+public class Inventory extends JFrame
 {
-
+    //Main section
+    JFrame frame = new JFrame("Inventory");
+    JTabbedPane tabs = new JTabbedPane();
+    
+    JPanel consumableTab = new JPanel();
+    JPanel defenseTab = new JPanel();
+    JPanel weaponTab = new JPanel();
+    JPanel wearableTab = new JPanel();
+    
     
     
 }
